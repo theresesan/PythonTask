95f1b4c217580217cc8e0844964217ce56485b1e
Therese <116627028+theresesan@users.noreply.github.com>
2023-01-07 15:29:42 +0100

    Add files via upload

---
+++ pythonTask.py
@@ -0,0 +1,62 @@
+# Task 1
+
+# If else statment
+x = 10
+if x + 11 == 21:
+    print("correct")
+else:
+    print("false")
+
+# If statement
+num = 30
+if 20 + 10 == 30:
+    print("Correct answer")
+
+# Task 2
+
+a = 30
+if 20 % 10 == 30:
+    print("yes")
+else:
+    print("no")
+
+# Task 3
+
+nextA = 30
+sum = ""
+if 20 % 10 == 30:
+    sum = "yes"
+else:
+    sum = "no"
+
+    print(sum)
+
+
+# Task 4
+
+import random
+
+numList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+randomNum = random.choice(numList)
+if (randomNum <= 10):
+   print("Random number is : " + str(randomNum))
+
+# Task 5 (Denne skjønte jeg ikke helt)
+
+newNum = 1
+if (randomNum == newNum):
+   print(f"Random is ({str(randomNum)}) == My Number is ({str(newNum)})")
+else:
+    print(f"Random is ({str(randomNum)}) != My new number is ({str(newNum)})")
+ 
+
+
+
+
+
+
+
+
+
+
+    
